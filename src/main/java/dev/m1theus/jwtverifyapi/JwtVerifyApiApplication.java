c075122c7c743ce32f6d9a877cfd206c25f29972
Matheus Martins <mail@mmartins.dev>
2024-02-08 21:12:33 -0300

    add app scafolding

---
+++ src/main/java/dev/m1theus/jwtverifyapi/JwtVerifyApiApplication.java
@@ -0,0 +1,13 @@
+package dev.m1theus.jwtverifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtVerifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtVerifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/m1theus/jwtverifyapi/JwtVerifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.m1theus.jwtverifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtVerifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
